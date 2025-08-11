198adf0509c2428b35b22a6719d3809355198d27
Mech654 <Drahmet2007@hotmail.com>
2025-08-11 10:34:49 +0200

    New project + some other additions

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        stacktest2();
+        
+
+    }
+
+    private void stacktest1()
+    {
+        Human hm = new Human();
+        hm.Name = "John";
+
+        Human hm2 = hm;
+        Console.WriteLine(hm.Name);
+        Console.WriteLine(hm2.Name);
+        
+        hm.Name = "Jane";
+        Console.WriteLine(hm2.Name);
+    }
+
+    static void stacktest2()
+    {
+        String str1 = "123123";
+        String str2 = str1;
+        Console.WriteLine(str1);
+        Console.WriteLine(str2);
+
+        str1 = "woaaa";
+        Console.WriteLine(str2);
+    }
+}
+
+class Human{
+    
+    public string Name { get; set; }
+
+}
---
+++ Terningspil/Terningspil/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Terningspil;
+using System;
+class Program
+{
+    static Die[] dice = new Die[6];
+    static void Main(string[] args)
+    {
+        int count = 1;
+        while (true)
+        {
+            NewDices();
+            if (CheckNumberInAllDices(6))
+            {
+                Console.WriteLine(count);
+                break;
+            }
+            count++;
+        }
+    }
+
+    static bool CheckNumberInAllDices(int number)
+    {
+        for (int i = 0; i < dice.Length; i++)
+        {
+            if (dice[i].Value != number)  return false;
+        }
+        return true;
+    }
+
+    static void NewDices()
+    {
+        for (int i = 0; i < dice.Length; i++)
+        {
+            dice[i] = new Die();
+        }
+    }
+}
+
+class Die
+{
+    public int Value { get; set; }
+    public void Roll()
+    {
+        Random random = new Random();
+        Value = random.Next(1, 7);
+    }
+
+    public Die()
+    {
+        Roll();
+    }
+}
